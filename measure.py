61ec1ade2788a05145530fb39d059028d56a0a77
Renann <74214530+rgbaldov@users.noreply.github.com>
2025-09-25 10:24:24 +0800

    Update measure.py

--- measure.py
+++ measure.py
@@ -1,27 +1,31 @@
 import cv2
 import math
 
-img = cv2.imread("xray.jpeg")
+img = cv2.imread("brain.jpg")
 clone = img.copy()
 
 points = []
+
+pixels_per_cm = 15.45   # divide px/skull size (depends on your image/dpi)
 
 def click_event(event, x, y, flags, param):
     global points, img
     
     if event == cv2.EVENT_LBUTTONDOWN:
         points.append((x, y))
-
+        
         cv2.circle(img, (x, y), 5, (0, 0, 255), -1)
         
         if len(points) == 2:
             cv2.line(img, points[0], points[1], (0, 255, 0), 2)
             
-            length = math.dist(points[0], points[1])
+            length_px = math.dist(points[0], points[1])
             
-            cv2.putText(img, f"{length:.2f} px",
+            length_cm = length_px / pixels_per_cm
+            
+            cv2.putText(img, f"{length_cm:.2f} cm",
                         (points[0][0], points[0][1] - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.7,
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.8,
                         (255, 255, 255), 2)
             
             points = []
